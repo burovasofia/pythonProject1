cc4a1bf270634cadb236079d268d2a1fb2ef6e59
Sofia <burovasofia@list.ru>
2022-10-08 19:32:49 +0300

    seminar3

--- seminar3.py
+++ seminar3.py
@@ -22,5 +22,51 @@
 #            sum = sum + int(i)
 # print(sum)
 
-n = int(input('Введите размер списка '))
-list_original = list([random.randint(10,99) for i in range(0, n)])
+# Задайте список. Напишите программу, которая определит,
+# присутствует ли в заданном списке строк некое число.
+
+# n = int (input('Введите размер списка '))
+# import random
+# list = list([random.randint(1,10) for i in range(0, n)])
+# print(list)
+# a = 6
+# if a in list:
+#     list.remove(6)
+#     print(list)
+# else:
+#     print('Значения 6 в списке нет')
+
+
+# 21.	Напишите программу, которая определит позицию второго
+# вхождения строки в списке либо сообщит, что её нет.
+
+
+# list = ["qwe", "asd", "zxc", "ertqwe", "qwe"]
+# find = "qwe"
+# count = 0
+# for i in list:
+#     if list[i] == find:
+#         count = count+1
+#         print(i)
+
+
+
+# list = ["qwe", "asd", "zxc", "ertqwe", "qwe"]
+# find = "qwe"
+# print(list)
+# find = list[0]
+# count = 0
+# for i in range(len(list)):
+#     if list[i] == find:
+#         count+=1
+#         if count == 2:
+#             print("Второе вхождение числа", i)
+# Найти индекс повторяющего элемента
+# a = ['1', 'af', '6', 'a', '4', 'af', '3']
+# for i in range(len(a)):
+#     for j in range(i+1,len(a)):
+#         if(a[i]==a[j]):
+#             print(a[j])
+#             print(j)
+
+
