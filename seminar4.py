11df3e02e2804c47bf064df43537c44ff5c259ec
Sofia <burovasofia@list.ru>
2022-10-12 19:46:49 +0300

    домашка 5

--- seminar4.py
+++ seminar4.py
@@ -61,9 +61,10 @@
 # Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности 3n+1
 # Для n=6 {1:4, 2:7, 3:10, 4:13, 5:16, 6:19}
 
-n = int(input('введите число '))
-slovar = {}
-for i in range(1, n+1):
-    slovar.update({i:3*i+1})
-print(slovar)
+# n = int(input('введите число '))
+# slovar = {}
+# for i in range(1, n+1):
+#     slovar.update({i:3*i+1})
+# print(slovar)
+
 
