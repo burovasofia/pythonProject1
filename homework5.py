5194ae2ad469eeaad61b2b2637c9da2ff7a71f45
Sofia <burovasofia@list.ru>
2022-10-23 20:44:51 +0300

    задача

--- homework5.py
+++ homework5.py
@@ -115,15 +115,17 @@
 # и возвращает название сезона, к которому относится этот месяц.
 #Например, передаем 2, на выходе получаем 'Зима'.
 
-# def month_to_season():
-a = int(input('введите номер месяца '))
-if a == 1 or a == 2 or a == 12:
-    print("зима")
-elif a == 3 or a == 4 or a == 5:
-    print("весна")
-elif a == 6 or a == 7 or a == 8:
-    print("лето")
-elif a == 9 or a == 10 or a == 11:
-    print("осень")
-else:
-    print("укажите правильный номер месяца")
+def month_to_season(a):
+    if a == 1 or a == 2 or a == 12:
+        return ("зима")
+    elif a == 3 or a == 4 or a == 5:
+        return("весна")
+    elif a == 6 or a == 7 or a == 8:
+        return("лето")
+    elif a == 9 or a == 10 or a == 11:
+        return("осень")
+    else:
+        return("укажите правильный номер месяца")
+
+print(month_to_season(int(input('введите номер месяца '))))
+
